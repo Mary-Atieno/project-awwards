1774a61ccd7ce81e593c70bc59361d2ec52e939d
mary atieno <mercymary 948@gmail.com>
2022-06-14 16:58:43 +0300

    adds contact

---
+++ awards/forms.py
@@ -0,0 +1,29 @@
+from pyexpat import model
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from.models import *
+
+class NewProjectForm(forms.ModelForm):
+    
+    class Meta:
+        model = Project
+        fields = ('title', 'url', 'description', 'technologies','image','user',)
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+ 
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+ 
+ 
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image','bio',]
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Ratings
+        fields = ['design','usability','content','score',]
